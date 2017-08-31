7c04c42a0d00cd3c9bfe8b18dbf9595b1ea8a0f3
Ian Cobb <iancobb@Ians-MacBook-Pro.local>
2017-08-31 12:43:36 -0700

    Added guest user to current_user

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -14,7 +14,7 @@ def new
 end
 
 def create
-  @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+  @portfolio_item = Portfolio.new(portfolio_params)
 
   respond_to do |format|
     if @portfolio_item.save
@@ -33,7 +33,7 @@ def update
   @portfolio_item = Portfolio.find(params[:id])
 
   respond_to do |format|
-    if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+    if @portfolio_item.update(portfolio_params)
       format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
       format.json { render :show, status: :ok, location: @blog }
     else
@@ -59,4 +59,15 @@ def destroy
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, 
+                                      :subtitle, 
+                                      :body, 
+                                      technologies_attributes: [:name]
+                                      )
+  end  
+
 end
