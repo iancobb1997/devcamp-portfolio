c7619856074914a4cb8775371b521003f5531fe1
Ian Cobb <iancobb@Ians-MacBook-Pro.local>
2017-09-05 12:48:03 -0700

    Portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,7 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout "portfolio"
+
 	def index
    @portfolio_items = Portfolio.all
  end
@@ -27,13 +29,10 @@ def create
 end
 
 def edit
-  @portfolio_item = Portfolio.find(params[:id])
   3.times { @portfolio_item.technologies.build }
 end
 
 def update
-  @portfolio_item = Portfolio.find(params[:id])
-
   respond_to do |format|
     if @portfolio_item.update(portfolio_params)
       format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
@@ -46,13 +45,9 @@ def update
 end
 
 def show
-  @portfolio_item = Portfolio.find(params[:id])
 end
 
 def destroy
-    #Perform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
-
     #Destroy/delete the record
     @portfolio_item.destroy
 
@@ -71,5 +66,8 @@ def destroy
                                       technologies_attributes: [:name]
                                       )
   end  
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
 end
